e85f38a3c8980e9ed8cf3f704a2af58fe9e1f9d1
王二 <18530117331@163.com>
2024-11-07 23:11:46 +0800

    包传上去

---
+++ Packages/cn.etetet.yiuiframework/DotNet~/SourceGenerator/Config/YIUIDefinition.cs
@@ -0,0 +1,12 @@
+﻿namespace ET
+{
+    public static class YIUIDiagnosticCategories
+    {
+        public const string Model = "YIUIETModelProjectAnalyzers";
+    }
+
+    public static class YIUIDiagnosticIds
+    {
+        public const string YIUIEntitySystemAnalyzerRuleId = "YIUI0001";
+    }
+}
